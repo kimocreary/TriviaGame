26d153f790273ba4ff5a7583e059c1bc9b8141ad
Kimberlyn Creary <kimocreary@gmail.com>
2019-09-10 20:17:46 -0400

    links and questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+var quizQuestions = [
+    {
+        question: What is the first full length CGI movie?
+        choices: [ "A Bug's Life", "Monsters Inc", "Toy Story", "The Lion King"]
+
+        Which of these is Not a name of one of the Spice Girls?
+
+        Which NBA team won the most titles in the 90's?
+
+        Which group released the hit song, "Smells Like Teen Spirit?"
+    
+
+    }
+]
+
+
+
+
+// User clicks the start button
+
+    // Start page disappears
+    // Questions and Answers appear
+    // Timer starts
+// Timer starts
+// User answers questions
+// When timer reaches zero, game ends
+    // Timer goes away
+    // Game scores the questions
+    // Questions disappear
+// Correct answers and wrong answers displayed
+
+ 
+
+
+
